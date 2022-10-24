4f6127d655f0631d065914e770321c12f0a28301
alizeehamon <alizee.hamonpro@gmail.com>
2022-10-24 11:59:35 +0200

    add graph dashboard

--- src/main/java/com/example/rosacrm/repository/NoteRepository.java
+++ src/main/java/com/example/rosacrm/repository/NoteRepository.java
@@ -1,9 +1,18 @@
 package com.example.rosacrm.repository;
 
 import com.example.rosacrm.entity.Note;
+import com.example.rosacrm.entity.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface NoteRepository extends CrudRepository<Note, Long> {
+    @Query("select n from Note n where n.client.user = ?1")
+    List<Note> findClientNoteByUser(User user);
+
+    @Query("select n from Note n where n.prospect.user = ?1")
+    List<Note> findProspectNoteByUser(User user);
 }
